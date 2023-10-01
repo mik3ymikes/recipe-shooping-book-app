2a265cfab3e6af8d7d2cd1450533c023e62b8798
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-30 22:56:27 -0500

    Completed lessons up to looping with ngFor

--- basic/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basic/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipes.model"
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[]=[
+    new Recipe("A test recipe", "simply a test","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcPBKGZDo0rHbKJ6JBGCWeKgLBV3BJsZy_WA&usqp=CAU" ),
+    new Recipe("A test recipe", "simply a test","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcPBKGZDo0rHbKJ6JBGCWeKgLBV3BJsZy_WA&usqp=CAU" )
+  ]
 
 }
