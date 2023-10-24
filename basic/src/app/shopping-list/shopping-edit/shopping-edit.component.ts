7733d2d0cd6d51626b4e0664cbdbafa1d978278a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-24 15:09:50 -0500

    up to allowin the deletion of shopping list items

--- basic/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ basic/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -55,6 +55,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   }
 
+  onClear(){
+    this.slForm.reset()
+    this.editMode=false
+  }
+
 
   ngOnDestroy(): void {
       this.subscription.unsubscribe()
