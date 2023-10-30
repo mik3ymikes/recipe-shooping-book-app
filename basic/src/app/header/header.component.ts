f5bf09a66b199ae77f7d0dc471c5c5d982dbdf56
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-30 15:06:09 -0500

    completed up to fetching recipes

--- basic/src/app/header/header.component.ts
+++ basic/src/app/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core'
 
+import { DatastorageService } from '../shared/data-storage-service'
+
 @Component({
   selector:'app-header',
   templateUrl:'./header.component.html'
 })
 
 export class HeaderComponent{
+  constructor(private dataStorageService : DatastorageService) {}
+
+  onSaveData(){
+   this.dataStorageService.storeRecipes()
+  }
 
 
 
--- basic/src/app/shared/data-storage-service.ts
+++ basic/src/app/shared/data-storage-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
 
 
 @Injectable({providedIn: 'root'})
@@ -7,5 +8,13 @@ import { HttpClient } from "@angular/common/http";
 
 
 export class DatastorageService{
-  constructor (private http: HttpClient) {}
+  constructor (private http: HttpClient, private recipesService: RecipeService) {}
+
+
+  storeRecipes(){
+   const recipes=this.recipesService.getRecipes()
+   this.http.put('https://ng-course-recipe-book-2594f-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response =>{
+    console.log(response)
+   })
+  }
 }
