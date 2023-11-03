c6de5068382023f6d221405035091b7df96d1c11
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-03 14:07:14 -0500

    added auth page for recipe component

--- basic/src/app/app-routing.module.ts
+++ basic/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.com
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'
 import { RecipesResolverService } from './recipes/recipes-resolver.service'
+import { AuthComponent } from './auth/auth.component'
 
 const appRoutes:Routes=[
   {path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -23,6 +24,7 @@ const appRoutes:Routes=[
 
 ]  },
   {path: 'shopping-list' , component: ShoppingListComponent },
+  {path: 'auth', component:AuthComponent}
 
 ];
 
