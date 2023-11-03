480d5c274ff8f1efc28d100afa392a21590fe384
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-03 15:28:32 -0500

    completed sending a signup request

--- basic/src/app/auth/auth.component.ts
+++ basic/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 
 import { Component}  from '@angular/core'
 import { NgForm } from '@angular/forms'
+import { AuthService } from './auth.service'
 
 @Component({
   selector: 'app-auth',
@@ -9,7 +10,10 @@ import { NgForm } from '@angular/forms'
 
 
 
+
+
 export class AuthComponent{
+  constructor(private authService: AuthService) {}
   isLoginMode=true
 
   onSwitchMode(){
@@ -18,7 +22,29 @@ export class AuthComponent{
 
 
     onSubmit(form: NgForm){
-      console.log(form.value)
+      if(!form.valid){
+        return
+      }
+
+      const email=form.value.email
+      const password=form.value.password
+
+      if (this.isLoginMode){
+
+      }else{
+        this.authService.signup(email, password).subscribe(
+          resData => {
+          console.log(resData)
+        },
+        error => {
+          console.log(error)
+        }
+        )
+
+      }
+
+
+
       form.reset()
   }
 
