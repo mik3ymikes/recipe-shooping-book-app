b560eb8571e20baca6c9eaab7d2352e0756d9afd
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-05 17:17:12 -0600

    completed sending login request

--- basic/src/app/auth/auth.service.ts
+++ basic/src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData{
+ export interface AuthResponseData{
   kind:string;
   idToken:string;
   email:string;
   refreshToken:string;
   expiresIn:string;
-  localId:string
+  localId:string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root'})
@@ -37,5 +38,16 @@ export class AuthService {
       }
       return throwError(errorMessage)
     }))
+  }
+
+  login(email:string, password:string){
+    return this.http.post <AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBzmZnMqzdkpyz6KQpzIXPkSuRmtIE-It8',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+      )
   }
 }
