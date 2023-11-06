8942307ef3893e73e41fb15a8c87b71e9051a442
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-05 21:41:53 -0600

    completed adding auto logout part 2

--- basic/src/app/auth/auth.service.ts
+++ basic/src/app/auth/auth.service.ts
@@ -18,6 +18,7 @@ import { Router } from "@angular/router";
 @Injectable({ providedIn: 'root'})
 export class AuthService {
   user= new BehaviorSubject<User>(null)
+  private tokenExpirationTimer: any
 
 
   constructor(private http: HttpClient, private router: Router){}
@@ -69,15 +70,32 @@ export class AuthService {
   if(!userData) {
     return
   }
+
   const laodedUser=new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
-   if (laodedUser.token){
+
+  if (laodedUser.token){
+
     this.user.next(laodedUser)
+    const expirationDuration=new Date(userData._tokenExpirationDate).getTime()-
+    new Date(). getTime()
+    this.autoLogout(expirationDuration)
    }
  }
 
   logout(){
     this.user.next(null)
     this.router.navigate(['/auth'])
+    localStorage.removeItem('userData')
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer=null
+  }
+
+  autoLogout(expirationDuration:number){
+    this.tokenExpirationTimer=  setTimeout(() => {
+        this.logout()
+      } , expirationDuration)
   }
 
 private handleAuthentication(email:string, userId:string, token:string, expiresIn: number){
@@ -89,6 +107,7 @@ private handleAuthentication(email:string, userId:string, token:string, expiresI
     expirationDate
     )
     this.user.next(user)
+    this.autoLogout(expiresIn *1000)
     localStorage.setItem('userData', JSON.stringify(user))
 }
 
