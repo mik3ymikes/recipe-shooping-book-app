8f00c546e51414f15f334049f824fdfe43feef25
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-05 18:54:08 -0600

    completed adding the token to the outgoing requests

--- basic/src/app/shared/data-storage-service.ts
+++ basic/src/app/shared/data-storage-service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipes.model";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 
 
@@ -11,7 +12,7 @@ import { map, tap } from "rxjs/operators";
 
 
 export class DatastorageService{
-  constructor (private http: HttpClient, private recipesService: RecipeService) {}
+  constructor (private http: HttpClient, private recipesService: RecipeService, private authService: AuthService) {}
 
 
   storeRecipes(){
@@ -22,18 +23,31 @@ export class DatastorageService{
   }
 
   fetchRecipes() {
-   return this.http.get<Recipe[]>('https://ng-course-recipe-book-2594f-default-rtdb.firebaseio.com/recipes.json')
-    .pipe(map(recipes =>{
-      return recipes.map(recipe =>{
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+     return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user =>{
+      return this.http.get<Recipe[]>(
+        'https://ng-course-recipe-book-2594f-default-rtdb.firebaseio.com/recipes.json',
+        {
+          params: new HttpParams().set('auth', user.token)
+        }
+        )
+    }),
+      map(recipes =>{
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
         }
 
       })
     }),
-   tap( recipes =>{
+   tap(recipes =>{
      this.recipesService.setRecipes(recipes)
 
-   })
-    )
+   }) )
+
+
+
   }
 }
