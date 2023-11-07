396115a96a75d24b60ab0786f4b176c9a41d4efd
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-07 14:58:56 -0600

    completed data binding and event binding

--- basic/src/app/auth/auth.component.ts
+++ basic/src/app/auth/auth.component.ts
@@ -1,8 +1,8 @@
 
-import { Component, ComponentFactoryResolver, ViewChild}  from '@angular/core'
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild}  from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { AuthResponseData, AuthService } from './auth.service'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import { Router } from '@angular/router'
 
 import { AlertComponent } from '../alert/alert.component'
@@ -17,12 +17,14 @@ import { PlaceholderDirective } from '../shared/placeholder/placeholder.directiv
 
 
 
-export class AuthComponent{
+export class AuthComponent implements OnDestroy{
   constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
   isLoginMode=true
   isLoading= false
   error:string=null
   @ViewChild(PlaceholderDirective) alertHost:PlaceholderDirective
+
+  private closeSub: Subscription
 
   onSwitchMode(){
     this.isLoginMode=!this.isLoginMode
@@ -71,6 +73,12 @@ export class AuthComponent{
     this.error=null
   }
 
+  ngOnDestroy(): void {
+      if(this.closeSub){
+        this.closeSub.unsubscribe()
+      }
+  }
+
 
   private showErrorAlert(message:string){
    const alertCmpFactory=this.componentFactoryResolver.resolveComponentFactory(AlertComponent)
@@ -78,8 +86,16 @@ export class AuthComponent{
    const hostViewContainerRef=this.alertHost.viewContainerRef
    hostViewContainerRef.clear()
 
-   hostViewContainerRef.createComponent(alertCmpFactory)
+   const componentRef=hostViewContainerRef.createComponent(alertCmpFactory)
+
+   componentRef.instance.message=message
+  this.closeSub= componentRef.instance.close.subscribe(() =>{
+    this.closeSub.unsubscribe()
+    hostViewContainerRef.clear()
+
+   })
   }
+
 
 }
 
