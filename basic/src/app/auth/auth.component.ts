488f41fba97afc254e7686398e93781265725587
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-07 14:13:20 -0600

    completed preparing programmattic creation

--- basic/src/app/auth/auth.component.ts
+++ basic/src/app/auth/auth.component.ts
@@ -1,9 +1,11 @@
 
-import { Component}  from '@angular/core'
+import { Component, ComponentFactoryResolver}  from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { AuthResponseData, AuthService } from './auth.service'
 import { Observable } from 'rxjs'
 import { Router } from '@angular/router'
+
+import { AlertComponent } from '../alert/alert.component'
 
 @Component({
   selector: 'app-auth',
@@ -15,7 +17,7 @@ import { Router } from '@angular/router'
 
 
 export class AuthComponent{
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
   isLoginMode=true
   isLoading= false
   error:string=null
@@ -54,6 +56,7 @@ export class AuthComponent{
     errorMessage => {
       console.log(errorMessage)
       this.error=errorMessage
+      this.showErrorAlert(errorMessage)
 
       this.isLoading=false
     }
@@ -64,6 +67,12 @@ export class AuthComponent{
 
   onHandleError(){
     this.error=null
+  }
+
+
+  private showErrorAlert(message:string){
+   const alertCmpFactory=this.componentFactoryResolver.resolveComponentFactory(AlertComponent)
+   AlertComponent
   }
 
 }
