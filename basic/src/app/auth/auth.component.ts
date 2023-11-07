3cb10096e61aa7e583dad4ad2bd641c777d8bf36
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-07 14:42:01 -0600

    completed  creating a component programmatically

--- basic/src/app/auth/auth.component.ts
+++ basic/src/app/auth/auth.component.ts
@@ -1,11 +1,12 @@
 
-import { Component, ComponentFactoryResolver}  from '@angular/core'
+import { Component, ComponentFactoryResolver, ViewChild}  from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { AuthResponseData, AuthService } from './auth.service'
 import { Observable } from 'rxjs'
 import { Router } from '@angular/router'
 
 import { AlertComponent } from '../alert/alert.component'
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive'
 
 @Component({
   selector: 'app-auth',
@@ -21,6 +22,7 @@ export class AuthComponent{
   isLoginMode=true
   isLoading= false
   error:string=null
+  @ViewChild(PlaceholderDirective) alertHost:PlaceholderDirective
 
   onSwitchMode(){
     this.isLoginMode=!this.isLoginMode
@@ -73,6 +75,10 @@ export class AuthComponent{
   private showErrorAlert(message:string){
    const alertCmpFactory=this.componentFactoryResolver.resolveComponentFactory(AlertComponent)
    AlertComponent
+   const hostViewContainerRef=this.alertHost.viewContainerRef
+   hostViewContainerRef.clear()
+
+   hostViewContainerRef.createComponent(alertCmpFactory)
   }
 
 }
